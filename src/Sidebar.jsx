afb8c81a00900ccf4bf5b1bcf0b0f37d15ff4b56
Anvar <eandpiareinfinity@gmail.com>
2025-12-15 03:53:11 +0500

    MQ

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import "./Sidebar.css";
 import "./Content.css";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function Sidebar() {
+	useEffect(() => {
+		window.scrollTo(0, 0); // sahifa ochilganda tepaga qaytaradi
+	}, []);
+
 	return (
 		<div className="ota_sidebar umumiy">
 			<div className="sticky_qilinadigan">
